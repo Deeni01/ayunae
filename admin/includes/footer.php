3a327fa50f4dc70551edee26f7dd60a04ec1b322
undeviant <119622310+antideviant@users.noreply.github.com>
2023-06-25 01:32:13 +0800

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,19 @@
+<style>
+    .shopping-portal {
+        color: blue;
+    }
+
+    .year {
+        color: red;
+        margin-left: 10px; /* Adjust the value to add desired space */
+    }
+</style>
+
+<footer class="py-4 bg-light mt-auto">
+    <div class="container-fluid px-4">
+        <div class="d-flex align-items-center justify-content-center small">
+            <div class="text-muted shopping-portal">&copy; Shopping Portal</div>
+            <div class="year"><?php echo date('Y')?></div>
+        </div>
+    </div>
+</footer>
