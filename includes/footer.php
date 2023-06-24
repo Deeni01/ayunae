e43cc4c2ed798994c0b37fe7d4320c226017a89d
undeviant <119622310+antideviant@users.noreply.github.com>
2023-06-25 01:50:13 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,63 @@
+<div style='padding: 10px 20px 0px; border-top: dotted 1px #c28163;'>
+    <a href="#" style="position: fixed; bottom: 20px; left: 20px; padding: 10px; background-color: #ffffff; color: #c28163; border-radius: 50%; text-decoration: none; display: inline-block; width: 40px; height: 40px; text-align: center; line-height: 1; border: 2px solid #c28163;">
+        <span style="display: inline-block; width: 100%; text-align: center; vertical-align: middle; line-height: normal;">^</span>
+    </a>
+</div>
+
+<footer class="py-5 bg-brown">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 text-center text-white">
+            <h5 style="font-weight: bold; text-align: left;">AYUNAE</h5>
+                <p style="text-align: left;">
+                    Ayunae: Mother-Daughter Fashion Journey.<br>Raya brand clothing that blends tradition<br>and contemporary style with joy.
+                    <br>Experience elegance, inclusivity, and timeless classics inspired by Malaysia's beauty.
+                </p>
+            </div>
+            <div class="col-lg-4 text-center text-white">
+                <h5 style="font-weight: bold; text-align: left;">Follow Us</h5><br>
+                <div style="display: flex; justify-content: left;">
+                    <a href="https://www.instagram.com/ayunae.co/">
+                        <img src="assets/ig.png" alt="Instagram Logo" style="height: 50px; width: 50px; margin-right: 10px;">
+                    </a>
+                    <a href="https://www.facebook.com/ayunae.co">
+                        <img src="assets/fb.png" alt="Facebook Logo" style="height: 50px; width: 50px; margin-left: 10px;">
+                    </a>
+                    
+                    <a href="https://wa.me/60162166057">
+                        <img src="assets/ws.png" alt="Whatsapp Logo" style="height: 50px; width: 50px; margin-left: 10px;">
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 text-white">
+                <h5 style="font-weight: bold;">Customer Services</h5>
+                <p>
+                <p>
+                    <img src="assets/time.png" alt="Business Time" style="height: 20px; width: 20px; margin-right: 5px;">
+                    Monday - Saturday (8 a.m - 8 p.m)
+                </p>
+                <p>
+                    <a href="mailto:ayunaeco@gmail.com" style="text-decoration: none; color: inherit;">
+                        <img src="assets/email.png" alt="Email" style="height: 20px; width: 20px; margin-right: 5px;">
+                        ayunaeco@gmail.com
+                    </a>
+                </p>
+                <p>
+                    <a href="https://wa.me/60162166057" style="text-decoration: none; color: inherit;">
+                        <img src="assets/call.png" alt="Customer Service" style="height: 20px; width: 20px; margin-right: 5px;">
+                        016-216 6057
+                    </a>
+                </p>
+            </div>
+
+        </div>
+        <hr/>
+        <p class="m-0 text-center text-white">Copyright 2023 © Ayunae.co</p>
+    </div>
+</footer>
+
+<style>
+    .bg-brown {
+        background-color: #c28163 !important;
+    }
+</style>
